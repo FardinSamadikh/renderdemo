1eb704877638db457d0a5000b1e8c8c7cc8cc97e
Fardin Samadi Khoshe Mehr <119680166+FardinSamadikh@users.noreply.github.com>
2024-02-07 20:52:31 -0800

    Update percentile.py

--- percentile.py
+++ percentile.py
@@ -3,14 +3,6 @@
 
 # In[17]:
 
-
-import pandas as pd
-import numpy as np
-import dash
-from dash import dcc,html
-from dash.dependencies import Input, Output
-import plotly.graph_objs as go
-
 import pandas as pd
 import numpy as np
 import dash
